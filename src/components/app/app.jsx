d16a744fd51eeb492210aa724e0db3537790c539
rook-a <zhernovkov_konst@mail.ru>
2022-04-12 17:26:21 +0300

    refactor: обновит пакеты. восстановит работу приложения

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,5 @@
 import { ThemeProvider } from 'styled-components';
-import {
-  Switch,
-  Route,
-  BrowserRouter as Router,
-} from 'components/common/common';
+import { Routes, Route, BrowserRouter } from 'components/common/common';
 import DetailedQuest from 'components/detailed-quest/detailed-quest';
 import Contacts from 'components/contacts/contacts';
 import Home from 'components/home/home';
@@ -13,19 +9,14 @@ import * as S from './app.styled';
 const App = () => (
   <ThemeProvider theme={appTheme}>
     <S.GlobalStyle />
-    <Router>
-      <Switch>
-        <Route exact path="/quest">
-          <DetailedQuest />
-        </Route>
-        <Route exact path="/contacts">
-          <Contacts />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/quest" element={<DetailedQuest />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="*" element={<h1> not found </h1>} />
+      </Routes>
+    </BrowserRouter>
   </ThemeProvider>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from 'components/app/app';
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root'),
 );
