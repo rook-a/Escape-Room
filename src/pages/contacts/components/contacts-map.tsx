f40c3429b1f71e6a16b85207d4d4614a4289b9a8
rook-a <zhernovkov_konst@mail.ru>
2022-04-16 16:27:33 +0300

    feat: реализует карту через react-leaflet

--- src/pages/contacts/components/contacts-map.tsx
+++ src/pages/contacts/components/contacts-map.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useRef } from 'react';
-
-import useMap from 'src/hooks/use-map';
-import { Icon, Marker } from 'leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
 
 import { LAT, LNG } from 'src/utils/const';
 import DefaultPin from '../../../assets/img/icon-blip.svg';
 import 'leaflet/dist/leaflet.css';
 import * as S from './contacts-map.styled';
+
+const LAYER_URL =
+  'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
+const LAYER_COPYRIGHT =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
+const ZOOM = 17;
 
 const defaultIcon = new Icon({
   iconUrl: DefaultPin,
@@ -14,38 +18,24 @@ const defaultIcon = new Icon({
   iconAnchor: [20, 25],
 });
 
-const defaultIconSmall = new Icon({
-  iconUrl: DefaultPin,
-  iconSize: [46, 60],
-  iconAnchor: [18, 15],
-});
+const position = {
+  lat: LAT,
+  lng: LNG,
+};
 
 const ContactsMap = () => {
-  const mapRef = useRef(null);
-  const map = useMap(mapRef);
-
-  useEffect(() => {
-    if (map) {
-      const marker = new Marker({
-        lat: LAT,
-        lng: LNG,
-      });
-
-      marker.setIcon(defaultIcon).addTo(map);
-
-      map.on('zoomend', () => {
-        const currentZoom = map.getZoom();
-
-        if (currentZoom < 17) {
-          marker.setIcon(defaultIconSmall);
-        } else {
-          marker.setIcon(defaultIcon);
-        }
-      });
-    }
-  }, [map]);
-
-  return <S.ContactsMap ref={mapRef} />;
+  return (
+    <S.ContactsMap>
+      <MapContainer
+        center={position}
+        zoom={ZOOM}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer url={LAYER_URL} attribution={LAYER_COPYRIGHT} />
+        <Marker position={position} icon={defaultIcon} />
+      </MapContainer>
+    </S.ContactsMap>
+  );
 };
 
 export default ContactsMap;
